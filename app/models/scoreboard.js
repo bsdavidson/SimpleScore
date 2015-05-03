c52afda8e4195c1227b517c1ef7afe3805996e63
Brian Davidson <bsdavidson@gmail.com>
2015-05-02 23:40:12 -0500

    Added new routes for getting and settings scores.

--- app/models/scoreboard.js
+++ app/models/scoreboard.js
@@ -1,8 +1,12 @@
 var mongoose = require('mongoose');
-var Schema = mongoose.Scheme;
+var Schema = mongoose.Schema;
 
 var ScoreSchema = new Schema({
-    name: String
+    serverId: String,
+    gameId: String,
+    gameDate: { type: Date, default: Date.now },
+    playerName: String,
+    score: { type: Number, min: 0, max: 99999999999 }
 });
 
 module.exports = mongoose.model('Score', ScoreSchema);
--- server.js
+++ server.js
@@ -2,22 +2,97 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+// CONFIG - Define a dbserver.js file with the following syntax:
+//
+// var Server =  {
+//   'username': 'mongoDBusername',
+//   'password': 'mongoDBpassword',
+//   'server': 'mongoDBserverHost'
+// };
+//
+// module.exports = Server;
+var config = require('./dbserver');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://' + config.username + ':' + config.password + '@' + config.server);
+
+var Score = require('./app/models/scoreboard');
 
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
-
+// Routes ===================
 var router = express.Router();
+
+
+// Do this on every request
+router.use(function(req, res, next) {
+  console.log('I got a request');
+  next();
+});
+
 
 router.get('/', function(req, res) {
   res.json({ message: 'Your a winner!' });
 });
 
-// more routes
+// Scores Route
+router.route('/scores')
 
-// prefix
+  // POST ROUTE
+  .post(function(req, res) {
+    var score = new Score();
+    score.serverId = req.body.serverId;
+    score.gameId = req.body.gameId;
+    score.playerName  = req.body.playerName;
+    score.score = req.body.score;
+    score.save(function(err) {
+      if (err)
+        res.send(err);
+      res.json({ message: 'Score created!' });
+    });
+  })
+
+  // GET ALL SCORES
+  .get(function(req, res) {
+    Score.find(function(err, scores) {
+      if (err)
+        res.send(err);
+
+      res.json(scores);
+
+    });
+  });
+
+// Get all scores from specific server
+router.route('/scores/:server_id')
+
+  .get(function(req, res) {
+    Score.find({serverId: req.params.server_id}, function(err, scores) {
+      if (err)
+        res.send(err);
+      res.json(scores);
+    });
+
+  });
+
+// Get scores from specific server and game
+router.route('/scores/:server_id/:game_id')
+
+  .get(function(req, res) {
+    Score.find({serverId: req.params.server_id, gameId: req.params.game_id}, function(err, scores) {
+      if (err)
+        res.send(err);
+      res.json(scores);
+    });
+
+  });
+
+
+// Register Routes ====================
+
 app.use('/api', router);
 
 // Start
